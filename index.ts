ff50b613ece96c30acae5681649cf27c27d76079
Anwesh43 <anweshthecool0@gmail.com>
2021-04-16 23:38:30 +0530

    adding VBNode class

--- index.ts
+++ index.ts
@@ -146,3 +146,45 @@ class Animator {
         }
     }
 }
+
+class VBNode {
+
+    prev : VBNode 
+    next : VBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new VBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawVBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : VBNode {
+        var curr : VBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
