07f95b44d27a7dc27cd3e32db255a4fdc0bc79c4
Anwesh43 <anweshthecool0@gmail.com>
2021-04-16 23:40:10 +0530

    adding VBar class

--- index.ts
+++ index.ts
@@ -188,3 +188,26 @@ class VBNode {
         return this 
     }
 }
+
+class VBar {
+
+    curr : VBNode = new VBNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
